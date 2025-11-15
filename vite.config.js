a2f0149c362295ca3f3c131ae2d1afb9ee7b2771
azhar0i0 <azharisworking@gmail.com>
2025-11-15 16:14:29 +0500

    Fix Vite base path and SPA rewrites for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // important for Vercel static deploy
   plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
@@ -13,4 +13,7 @@ export default defineConfig({
       '@pages': path.resolve(__dirname, 'src/pages'),
     },
   },
+  build: {
+    outDir: 'dist'
+  }
 })
